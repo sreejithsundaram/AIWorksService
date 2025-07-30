40e2fbbbe3be31a9c6f20f0fc87f53653f0e7a8d
sreejithsundaram <sreejithsundaram@outlook.com>
2025-07-30 14:22:35 +0530

    Add project files.

---
+++ AIWorksService/Controllers/WeatherForecastController.cs
@@ -0,0 +1,49 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace AIWorksService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly IConfiguration _configuration;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public async Task<IEnumerable<WeatherForecast>> Get()
+        {
+            string result = string.Empty;
+            using (HttpClient client = new HttpClient())
+            {
+                string? url = _configuration["WORKS_API"];
+                if (url != null)
+                {
+                    var response = await client.GetAsync(url);
+                    if (response != null) 
+                    { 
+                        result = await response.Content.ReadAsStringAsync();
+                    }
+                }
+            }
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)],
+                Greeting = result
+            })
+            .ToArray();
+        }
+    }
+}
